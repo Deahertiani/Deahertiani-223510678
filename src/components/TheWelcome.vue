32b454008c8109cd30a2790b53b80187e93688a7
Deahertiani <deahertiani@student.uir.ac.id>
2024-04-04 12:42:20 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Pengalaman</template>
+
+   saya pernah magang di dinas komunikasi dan informasi.
+
+    <br />
+
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Contact</template>
+
+    Email: deahertiani@student.uir.ac.id <br>
+    Instagram : <a href="instagram.com/deahertiani">@deahertiani</a> <br>
+    WhatsApp : 0852-6491-6907
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Skill</template>
+
+    1. Kolaborasi <br>
+    2. Keterampilan komunikasi <br>
+    3. Cepat beradaptasi 
+  </WelcomeItem>
+</template>
